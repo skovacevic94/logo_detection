9f59576a0678e5f01d702601f6724e2f191212d1
Stanko Kovacevic <stanko.kovacevic994@gmail.com>
2021-07-04 19:44:15 +0200

    Added support for vocab svm split train

--- src/sift_bow_svm_classify.py
+++ src/sift_bow_svm_classify.py
@@ -3,13 +3,14 @@ from tqdm import tqdm
 import cv2
 import numpy as np
 from scipy.cluster.vq import kmeans, vq
+from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import LogisticRegression
 
-def create_vocabulary(sift, images, k=130, logos=None):
+def create_vocabulary(images, logos, sift, k=130, roi=False):
     all_descriptors_list = []
     for i, img in enumerate(tqdm(iterable=images, desc="Computing SIFT descriptors")):
-        if logos is not None:
+        if roi:
             bbox = logos[i][0]
             img = img[bbox.y:bbox.y+bbox.h, bbox.x:bbox.x+bbox.w]
         _, descriptors = sift.detectAndCompute(img, None)
@@ -24,12 +25,13 @@ def create_vocabulary(sift, images, k=130, logos=None):
     voc, variance = kmeans(all_descriptors, k, iter=1)
     return voc
 
-def compute_features(sift, voc, images, logos):
+def compute_features(images, logos, sift, voc, roi=False):
     X = np.zeros((len(images), len(voc)))
     y = np.zeros(len(images))
     for i, img in enumerate(tqdm(iterable=images, desc="Computing features (Quantization + Histogram)")):
-        bbox = logos[i][0]
-        img = img[bbox.y:bbox.y+bbox.h, bbox.x:bbox.x+bbox.w]
+        if roi:
+            bbox = logos[i][0]
+            img = img[bbox.y:bbox.y+bbox.h, bbox.x:bbox.x+bbox.w]
         _, desc = sift.detectAndCompute(img, None)
         if desc is None:
             continue
@@ -42,20 +44,27 @@ def compute_features(sift, voc, images, logos):
 if __name__=='__main__':
     sift = cv2.SIFT_create()
     clf = LinearSVC(C=0.01)
-    
+    voc_split = None # 0.4
+
     train_images, test_images, train_logos, test_logos = load_data('./data', test_size=0.33)
+    voc_images = train_images
+    voc_logos = train_logos
+    svm_images = train_images
+    svm_logos = train_logos
+    if voc_split is not None:
+        voc_images, svm_images, voc_logos, svm_logos = train_test_split(train_images, train_logos, train_size=voc_split, random_state=42)
     
     k = 3000
-    voc = create_vocabulary(sift, train_images, k, train_logos)
+    voc = create_vocabulary(voc_images, voc_logos, sift, k, False)
 
     print("Training stage...")
-    X_train, y_train = compute_features(sift, voc, train_images, train_logos)
+    X_train, y_train = compute_features(svm_images, svm_logos, sift, voc, False)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_train)
     report_metrics(y_train, y_pred, "Train")
 
     print("Evaluation stage...")
-    X_test, y_test = compute_features(sift, voc, test_images, test_logos)
+    X_test, y_test = compute_features(test_images, test_logos, sift, voc, False)
     y_pred = clf.predict(X_test)
     report_metrics(y_test, y_pred, "Test")
     
